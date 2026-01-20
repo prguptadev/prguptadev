bdf2fd68dd2dec360c8e1b70af95604568f5cce4
Prashant Gupta <pr.gupta1993@gmail.com>
2026-01-20 15:20:54 +0530

    Add ArmstrongNumber Java program and .gitignore

---
+++ ArmstrongNumber.java
@@ -0,0 +1,23 @@
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        int number = 153; // Example number
+        if (isArmstrong(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+    }
+
+    public static boolean isArmstrong(int number) {
+        int originalNumber, remainder, result = 0;
+        originalNumber = number;
+
+        while (originalNumber != 0) {
+            remainder = originalNumber % 10;
+            result += Math.pow(remainder, 3);
+            originalNumber /= 10;
+        }
+
+        return result == number;
+    }
+}
